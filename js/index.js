95b6aab4279ba84e7298d62dda2c1bd570ce99c7
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-05-18 19:40:02 +0600

    task: 3 added

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// task no 1: 1. Create a function that will find the factorial number of [9] using recursion.
+// task: 1 -> Create a function that will find the factorial number of [9] using recursion.
 // f(n-1) + n = f(n)
 
 function sum(n){
@@ -8,7 +8,7 @@ function sum(n){
         return sum(n-1) + n;
     }
 }
-sum(9)
+console.log(sum(9))
 /* 
 work example: how it work
 sum(4)
@@ -22,7 +22,7 @@ warning: recursion is not a optimized solution and not recommend because if i ca
 because call stack is fulled.that's why this is not recommended
 */
 
-// task: 2 - Find the maximum number in a jagged array given below array of numbers?
+// task: 2 -> Find the maximum number in a jagged array given below array of numbers?
 
 var arr = [2, 4, 10, [12, 40, [100, 99], 4],
     [3, 2, 99], 0
@@ -49,5 +49,28 @@ const largestNumber = function(arr){
     const maxNumberInArray = Math.max(...allNumberOfArray)
     return maxNumberInArray
 }
-largestNumber(arr)
+console.log(largestNumber(arr))
+
+
+// task: 3 -> Find the counts of duplicates in an array?
+
+const arr2 = ["1", "5", "9", "14", "5", "22", "48", "25", "22", "20", "9", "13"]
+const duplicatesCount = function(arr) {
+    const duplicate = []
+    for(let arrayIndex = 0;arrayIndex < arr.length;arrayIndex++)
+    {
+        if(arr.indexOf(arr[arrayIndex]) !== arrayIndex){
+            duplicate.push(arr[arrayIndex])
+        }
+    }
+    return {
+        duplicate,
+        duplicateCount: duplicate.length
+    }
+}
+console.log(duplicatesCount(arr2))
+
+// task: 3 -> How to find unique values from an array in sorted order?
+
+const arr = [1, 5, 7, 44, 5, 9, 4, 5, 9, 10, 6, 7, 8]
 
